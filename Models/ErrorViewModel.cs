3d3574d5ca1ab629cd1cb2a18ca7d9b6b76ebe50
AkhonaMaphumulo <akhonamaphumulo24@gmail.com>
2026-08-07 16:37:56 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreeFlow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
